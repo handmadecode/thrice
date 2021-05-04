0010ce9c7188f271b7c8566d2ce6d12d88a88674
Peter Franzen <peter@myire.org>
2021-05-04 08:27:54 +0200

    Add bytesToCompactHexString() and asString() without default value

--- src/main/java/org/myire/util/Strings.java
+++ src/main/java/org/myire/util/Strings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006, 2008-2009, 2011 Peter Franzen. All rights reserved.
+ * Copyright 2006, 2008-2009, 2011, 2021 Peter Franzen. All rights reserved.
  *
  * Licensed under the Apache License v2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
@@ -21,6 +21,12 @@ import org.myire.annotation.Unreachable;
  */
 public final class Strings
 {
+    static private final char[] HEX_CHARS = {
+        '0', '1', '2', '3', '4', '5', '6', '7',
+        '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
+
+
     /**
      * Private constructor to disallow instantiations of this class.
      */
@@ -32,7 +38,8 @@ public final class Strings
 
 
     /**
-     * Convert a sequence of bytes to a string of hexadecimal values.
+     * Convert a sequence of bytes to a string of hexadecimal values. The hexadecimal values will be
+     * on the form {@code 0xnn} and separated with spaces, e.g. &quot;0x82 0x01 0xfc&quot;.
      *
      * @param pBytes    A byte array with the bytes to convert.
      *
@@ -48,7 +55,8 @@ public final class Strings
 
 
     /**
-     * Convert a sequence of bytes to a string of hexadecimal values.
+     * Convert a sequence of bytes to a string of hexadecimal values. The hexadecimal values will be
+     * on the form {@code 0xnn} and separated with spaces, e.g. &quot;0x82 0x01 0xfc&quot;.
      *
      * @param pBytes    A byte array with the bytes to convert.
      * @param pOffset   The index of the first byte to get the hexadecimal value for.
@@ -90,6 +98,61 @@ public final class Strings
 
 
     /**
+     * Convert a sequence of bytes to a string of hexadecimal values. The hexadecimal values will be
+     * on the form {@code nn} and the values will be adjacent to each other without any separator,
+     * e.g. &quot;8201fc&quot;.
+     *
+     * @param pBytes    A byte array with the bytes to convert.
+     *
+     * @return  The bytes as hex values.
+     *
+     * @throws NullPointerException if {@code pBytes} is null.
+     */
+    @Nonnull
+    static public String bytesToCompactHexString(@Nonnull byte[] pBytes)
+    {
+        return bytesToCompactHexString(pBytes, 0, pBytes.length);
+    }
+
+
+    /**
+     * Convert a sequence of bytes to a string of hexadecimal values. The hexadecimal values will be
+     * on the form {@code nn} and the values will be adjacent to each other without any separator,
+     * e.g. &quot;8201fc&quot;.
+     *
+     * @param pBytes    A byte array with the bytes to convert.
+     * @param pOffset   The index of the first byte to get the hexadecimal value for.
+     * @param pLength   The number of bytes to get the hexadecimal values for.
+     *
+     * @return  The bytes as hex values.
+     *
+     * @throws NullPointerException         if {@code pBytes} is null.
+     * @throws IndexOutOfBoundsException    if {@code pOffset} is less than 0, or if
+     *                                      {@code pOffset + pLength} is greater than the size of
+     *                                      the array.
+     * @throws NegativeArraySizeException   if {@code pLength} is negative.
+     */
+    @Nonnull
+    static public String bytesToCompactHexString(
+        @Nonnull byte[] pBytes,
+        @Nonnegative int pOffset,
+        @Nonnegative int pLength)
+    {
+        if (pLength == 0)
+            return "";
+
+        // Each byte occupies two characters.
+        StringBuilder aBuffer = new StringBuilder(pLength * 2);
+
+        int aLastByte = pOffset + pLength;
+        for (int i=pOffset; i<aLastByte; i++)
+            appendByteAsCompactHex(aBuffer, pBytes[i]);
+
+        return aBuffer.toString();
+    }
+
+
+    /**
      * Append a byte on the format &quot;0xnn&quot; to a {@code StringBuilder}.
      *
      * @param pBuilder  The {@code StringBuilder} to append the byte to.
@@ -100,13 +163,24 @@ public final class Strings
     static private void appendByteAsHex(@Nonnull StringBuilder pBuilder, byte pByte)
     {
         pBuilder.append('0').append('x');
-        if (pByte >= 0x00 && pByte < 0x10)
-            pBuilder.append('0');
+        appendByteAsCompactHex(pBuilder, pByte);
+    }
 
-        // Integer.toHexString() does an implicit widening from byte to int, where negative bytes
-        // become negative ints, e.g. 0xff becomes 0xffffffff, which isn't the desired behaviour.
-        // Mask out all bits except the lowest 8 to avoid this.
-        pBuilder.append(Integer.toHexString(pByte & 0xff));
+
+    /**
+     * Append the hexadecimal value of a byte on the format &quot;nn&quot; to a
+     * {@code StringBuilder}.
+     *
+     * @param pBuilder  The {@code StringBuilder} to append the byte to.
+     * @param pByte     The byte to append.
+     *
+     *  @throws NullPointerException if {@code pBuilder} is null.
+     */
+    static private void appendByteAsCompactHex(@Nonnull StringBuilder pBuilder, byte pByte)
+    {
+        int aLowNibble = (pByte & 0x0f);
+        int aHighNibble = (pByte & 0xf0) >> 4;
+        pBuilder.append(HEX_CHARS[aHighNibble]).append(HEX_CHARS[aLowNibble]);
     }
 
 
@@ -145,6 +219,21 @@ public final class Strings
                 return false;
 
         return true;
+    }
+
+
+    /**
+     * Get the string representation of an object.
+     *
+     * @param pObject   The object to get the string representation of.
+     *
+     * @return  If {@code pObject} is non-null, {@code pObject.toString()} is returned, otherwise
+     *          {@code null} is returned.
+     */
+    @CheckForNull
+    static public String asString(@CheckForNull Object pObject)
+    {
+        return pObject != null ? pObject.toString() : null;
     }
 
 
